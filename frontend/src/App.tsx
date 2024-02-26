ff0dbc64d3fb2b10b1f44c178cbd41c8334bfa07
Benjamin Loidl <benni@loidlnetz.de>
2024-02-26 23:07:17 +0100

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+
+// {t('Welcome to React')}
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: {
+                    "Welcome to React": "Welcome to React and react-i18next"
+                }
+            }
+        },
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 function App() {
+    const { t } = useTranslation();
+
     return <BrowserRouter>
         <nav>
             <div>
@@ -12,21 +33,36 @@ function App() {
             <NavLink to="/a"><i className={ "fas fa-fire" }/>My Activity</NavLink>
             <NavLink to="/b"><i className={ "fas fa-star" }/>My Favorites</NavLink>
             <NavLink to="/c"><i className={ "fas fa-toolbox" }/>Daily Quests</NavLink>
-            <NavLink to="/d"><i className={ "fas fa-bell" }/>Inbox</NavLink>
+            <NavLink to="/d"><i className={ "fas fa-bell" }/>Inbox<span className={ "badge" }>3</span></NavLink>
         </nav>
-        
+
         <main>
             <div className={ "container top-bar" }>
-                <p className={ "search-bar" }><i className={ "fas fa-magnifying-glass" }/>Search..</p>
-                
-                <div style={ { display: "flex", flexDirection: "column" } }>
-                    <p>Welcome back,</p>
-                    <p>Benni</p>
+                <p className={ "search-bar" }>
+                    <i className={ "fas fa-magnifying-glass" }/>
+                    <span>Search..</span>
+                </p>
+
+                <div style={ {
+                    backgroundColor: "var(--background-color-primary)",
+                    borderRadius: "var(--border-radius)",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingInline: "var(--spacing)",
+                    gap: "var(--spacing)"
+                } }>
+                    <img className={ "avatar" }
+                         src={ "https://benniloidl.de/static/media/me.6c5597f7d72f68a1e83c.jpeg" }
+                         alt={ "Benni Loidl" }/>
+
+                    <p>
+                        Benni
+                        <span className={ "badge badge-sm" } style={ { marginInline: "var(--spacing)" } }>PRO</span>
+                    </p>
                 </div>
-                
-                <i className={ "fas fa-user-circle" }/>
             </div>
-            
+
             <Routes>
                 <Route path="/" element={ <div className={ "container" }><h1>Home</h1></div> }/>
                 <Route path="/about" element={ <h1>About</h1> }/>
