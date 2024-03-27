4b3ecf6dc47ecfe367ce98f6774106e0210f1a2b
Manuel Brauneis <manuel.brauneis@sap.com>
2024-03-27 14:04:46 +0100

    fixed login

--- backend/middleware/isLoggedin.ts
+++ backend/middleware/isLoggedin.ts
@@ -2,8 +2,5 @@ import {Request, Response, NextFunction} from "express";
 import { PrismaClient } from "@prisma/client";
 
 export default async function isLoggedIn(req: Request, res: Response, next: NextFunction) {
-    //let user_data = req.user;
-    //const prisma = new PrismaClient();
-    //const user = await prisma.user.findUnique({ where: { id: user_data.id, name: user_data.username } });
     req.user ? next() : res.sendStatus(401);
 }
