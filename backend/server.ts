85125cf0da46018d7271fa53550200278bd96113
Manuel Brauneis <manuel.brauneis@sap.com>
2024-03-27 19:36:11 +0100

    bug fix

--- backend/server.ts
+++ backend/server.ts
@@ -4,36 +4,21 @@ import passport from 'passport';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from '@prisma/client';
 import bodyParser from 'body-parser';
-
 require('dotenv').config()
 
-//middlewares
+//import middlewares
 require('./middleware/auth.ts');
 import isLoggedIn from './middleware/isLoggedin';
 import configSession from './middleware/configSession';
 import LoginWithCredentials from './api/auth/credentials/loginWithCredentials';
 
-//routes
+//import routes
 import HelloWorld from './api/test/route';
 
 const app = express();
 const port = 7070;
 configSession(app)
-  
-
 app.use(bodyParser.json());
-const sessionOptions: SessionOptions = {
-    secret: 'cats', 
-    resave: false, 
-    saveUninitialized: false,
-    store: new PrismaSessionStore(new PrismaClient(), {
-      checkPeriod: 2.4e+6,
-      dbRecordIdIsSessionId: true,
-    }),
-  };
-app.use(session(sessionOptions));
-app.use(passport.initialize());
-app.use(passport.session());
   
 
 app.get('/api/auth/google', passport.authenticate( 'google', {
