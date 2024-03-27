895aa46961fb177a1896585b0956b95a66d20c1e
Manuel Brauneis <manuel.brauneis@sap.com>
2024-03-27 13:00:31 +0100

    need to fix the isLoggedIn function/it is just checkin if user is in request but not in database

--- backend/middleware/isLoggedin.ts
+++ backend/middleware/isLoggedin.ts
@@ -1,5 +1,9 @@
 import {Request, Response, NextFunction} from "express";
+import { PrismaClient } from "@prisma/client";
 
-export default function isLoggedIn(req: Request, res: Response, next: NextFunction) {
+export default async function isLoggedIn(req: Request, res: Response, next: NextFunction) {
+    //let user_data = req.user;
+    //const prisma = new PrismaClient();
+    //const user = await prisma.user.findUnique({ where: { id: user_data.id, name: user_data.username } });
     req.user ? next() : res.sendStatus(401);
 }
