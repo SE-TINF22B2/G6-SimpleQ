c13f1881a9090b7d1f0761d269c385f776dd6c71
Manuel Brauneis <manuel.brauneis@sap.com>
2024-03-28 12:45:53 +0100

    fixed isLoggin function / needed to differentiate between prime, normal, etc

--- backend/middleware/isLoggedin.ts
+++ backend/middleware/isLoggedin.ts
@@ -1,6 +1,22 @@
 import {Request, Response, NextFunction} from "express";
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
 export default async function isLoggedIn(req: Request, res: Response, next: NextFunction) {
-    req.user ? next() : res.sendStatus(401);
+    interface myUser extends Express.User {
+        id: string,
+        displayName: string,
+        username: string,
+        profileUel: string,
+        photos: string[],
+    }
+    try{
+        let logged_user: myUser = req.user as myUser;
+        let user_exists = !!(await prisma.user.findUnique({where: {id: logged_user.id}}));
+        user_exists ? next() : res.sendStatus(401);
+    }
+    catch(err) {
+        res.sendStatus(401)
+    }
 }
