41e6198341a2477fee97e62647600824458c7dfa
MichalskyL <MichalskyL@gmx.de>
2024-04-24 15:56:16 +0200

    BE: added initialisation of Prisma and created model user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthMiddleware } from './middleware/auth/auth.middleware';
+import { PrismaService } from './database/prisma.service';
+import { UserService } from './database/user/user.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
