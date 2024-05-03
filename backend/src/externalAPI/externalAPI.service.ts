00fdbd007b499078003d7e26fc80bfebed7d5c20
Manuel Brauneis <manuel.brauneis@sap.com>
2024-05-03 10:39:21 +0200

    BE: external API

--- backend/src/externalAPI/externalAPI.service.ts
+++ backend/src/externalAPI/externalAPI.service.ts
@@ -2,16 +2,50 @@ import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { AxiosResponse } from "axios";
 import { Observable } from "rxjs";
+import OpenAI from 'openai';
 
 @Injectable()
 export class ExternalAPIService {
   constructor(private readonly httpService: HttpService) {}
 
-  requestWolfram(prompt: String): Observable<AxiosResponse<String>> {
-    return this.httpService.get('');
+  requestWolfram(prompt: string): Observable<AxiosResponse<String>> {
+    if(prompt === '') {
+      return null
+    }
+
+    let question = decodeURIComponent(prompt);
+
+    try {
+      let output = this.httpService.get(process.env['WOLFRAM_APP_ID']+question);
+      return output;
+    }
+    catch(error){
+      console.log("cannot send wolfram alpha request\n"+error)
+      return null;
+    }
   }
 
-  requestGPT(prompt: String): Observable<AxiosResponse<String>> {
-    return this.httpService.get('');
+  async requestGPT(prompt: string): Promise<string> {
+    if(prompt === '') {
+      return null
+    }
+
+    let question = decodeURIComponent(prompt);
+
+    const openai = new OpenAI({
+      apiKey: process.env['GPT_APP_ID'],
+    });
+
+    try{
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: question }],
+        model: 'gpt-3.5-turbo',
+      });
+      return chatCompletion[0];
+    }
+    catch(error){
+      console.log("cannot send AI request\n"+error)
+      return null;
+    }
   }
 }
