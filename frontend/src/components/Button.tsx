a48388dabf8173cd582c44913b8752b05037dd5c
Benjamin Loidl <benni@loidlnetz.de>
2024-05-13 10:51:56 +0200

    FE: Button Component v2

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./Button.scss";
 
 interface Props {
-	style: "primary" | "glass",
+	style?: "primary" | "glass",
 	icon: string,
-	onClick: () => void,
+	onClick?: () => void,
 	children: React.ReactNode,
-	disabled?: boolean
+	disabled?: boolean,
+	placeIconRight?: boolean
 }
 
 export default function Button(props: Props) {
 	const [isLoading, setIsLoading] = React.useState(false);
 	
 	return (
-		<button className={ "btn btn-" + props.style }
+		<button className={ "btn btn-" + (props.style ?? "glass") }
 				disabled={ props.disabled || isLoading }
 				onClick={ () => {
+					if (!props.onClick) return;
 					if (isLoading) return;
 					
 					setIsLoading(true);
@@ -24,8 +26,11 @@ export default function Button(props: Props) {
 					// Todo: Remove this timeout
 					setTimeout(() => setIsLoading(false), 1000);
 				} }>
-			<i className={ isLoading ? "fas fa-spinner" : props.icon }/>
+			{ !props.placeIconRight && <i className={ isLoading ? "fas fa-spinner" : props.icon }
+                                          style={ { marginRight: "var(--spacing)" } }/> }
 			<span>{ props.children }</span>
+			{ props.placeIconRight && <i className={ isLoading ? "fas fa-spinner" : props.icon }
+                                         style={ { marginLeft: "var(--spacing)" } }/> }
 		</button>
 	);
 }
