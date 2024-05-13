7cdfe1c107e43111bff17fc196b09c3b5511e315
Manuel Brauneis <manuel.brauneis@sap.com>
2024-05-13 14:27:29 +0200

    BE: merge main

--- backend/src/externalAPI/externalAPI.service.ts
+++ backend/src/externalAPI/externalAPI.service.ts
@@ -10,13 +10,17 @@ export class ExternalAPIService {
 
   async requestWolfram(prompt: string): Promise<string> {
     if(prompt === '') {
-      return null;
+      return "";
     }
 
     if(process.env.NODE_ENV === "dev"){
       return new Promise((resolve, rejects) => {
         resolve("Das ist nur eine default Antwort um Tokens zu sparen.")
       })
+    }
+
+    if(process.env.WOLFRAM_APP_ID == undefined) {
+      throw Error("ENV WOLFRAM_APP_ID is undefined")
     }
 
     const { data } = await firstValueFrom(
@@ -28,13 +32,13 @@ export class ExternalAPIService {
     }
     catch(error){
       console.log("Unable to convert response to base64\n\n"+error)
-      return null;
+      return "";
     }
   }
 
   async requestGPT(prompt: string): Promise<string> {
     if(prompt === '') {
-      return null
+      return ""
     }
 
     if(process.env.NODE_ENV === "dev"){
@@ -53,6 +57,9 @@ export class ExternalAPIService {
       }
     }
 
+    if(process.env.GPT_APP_URL == undefined){
+      throw Error("ENV GPT_APP_URL is undefined");
+    }
     const { data } = await firstValueFrom(
       this.httpService.post(process.env.GPT_APP_URL, body, header).pipe()
     );
@@ -61,7 +68,7 @@ export class ExternalAPIService {
       return data.output
     }
     else{
-      return null
+      return ""
     }
   }
 }
