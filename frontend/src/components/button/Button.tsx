29cc18adcdc5f51d0c71e3ca7a4977bdb7a72afb
Benjamin Loidl <benni@loidlnetz.de>
2024-05-15 08:39:53 +0200

    FE: Optimizing Button Component

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -25,10 +25,10 @@ export default function Button(props: Props) {
 					
 					setIsLoading(false);
 				} }>
-			{ props.icon && !props.placeIconRight && <i className={ isLoading ? "fas fa-spinner" : props.icon }
+			{ props.icon && !props.placeIconRight && <i className={ isLoading ? "fas fa-spinner fa-spin" : props.icon }
                                                         style={ { marginRight: "var(--spacing)" } }/> }
 			<span>{ props.children }</span>
-			{ props.icon && props.placeIconRight && <i className={ isLoading ? "fas fa-spinner" : props.icon }
+			{ props.icon && props.placeIconRight && <i className={ isLoading ? "fas fa-spinner fa-spin" : props.icon }
                                                        style={ { marginLeft: "var(--spacing)" } }/> }
 		</button>
 	);
