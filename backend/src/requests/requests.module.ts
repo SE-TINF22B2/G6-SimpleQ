ccb517b895b02916bd55df012a0b276b3f2f7684
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-05-16 15:48:17 +0200

    BE: import external services in request module

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -4,25 +4,43 @@ import {SpecialController} from "./special/special.controller";
 import {FavouritesController} from "./favourites/favourites.controller";
 import * as process from "process";
 import {DevelopmentController} from "./development/development.controller";
-import {Controller} from "@nestjs/common/interfaces";
 import {QuestsController} from "./quests/quests.controller";
 import {TagController} from "./tag/tag.controller";
 import {UserController} from "./user/user.controller";
 import {UserService} from "../database/user/user.service";
 import {PrismaService} from "../database/prisma.service";
-import {Expert} from "@prisma/client";
 import {ExpertService} from "../database/expert/expert.service";
+import {LoginAttemptService} from "../database/login-attempt/login-attempt.service";
+import {BlacklistService} from "../database/blacklist/blacklist.service";
+import {FavoriteService} from "../database/favorite/favorite.service";
+import {ModerationService} from "../database/moderation/moderation.service";
+import {QuestService} from "../database/quest/quest.service";
+import {TagService} from "../database/tag/tag.service";
 
 @Module({})
 export class RequestsModule {
     static register(): DynamicModule {
         const controllers : any[] = [
+            // controllers, which handle request traffic
             QuestionsController,
             FavouritesController,
             QuestsController,
             SpecialController,
             TagController,
             UserController
+        ]
+
+        const providers: any[] = [
+            // services, specialized and implemented in other folders
+            PrismaService,
+            BlacklistService,
+            ExpertService,
+            FavoriteService,
+            LoginAttemptService,
+            ModerationService,
+            QuestService,
+            TagService,
+            UserService,
         ]
         if (process.env.NODE_ENV === "dev"){
             console.log("Dev: DevelopmentController is loaded.")
@@ -32,8 +50,8 @@ export class RequestsModule {
         }
         return {
             module: RequestsModule,
-            providers: [PrismaService,UserService,ExpertService],
-            controllers
+            providers: providers,
+            controllers: controllers
         }
 
     }
