0286192f117de6af37568fdbccb2d92bafbbe7dc
Jozys <joshua@slaar.de>
2024-05-20 16:14:23 +0200

    BE: fixed imports in tests
    Signed-off-by: Jozys <joshua@slaar.de>

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,13 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../database/user/user.service';
+import { PrismaService } from '../database/prisma.service';
+import { mockPrisma } from '../database/mockedPrismaClient';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [AuthService, UserService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockPrisma)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
   });
