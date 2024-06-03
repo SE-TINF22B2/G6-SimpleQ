a5dccd636ae9fbed00acedda4d97774bec6a91af
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-06-03 23:31:41 +0200

    BE: Fixed tests for tags again
    Signed-off-by: Integraluminium <95375836+Integraluminium@users.noreply.github.com>

--- backend/src/requests/development/development.controller.ts
+++ backend/src/requests/development/development.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, NotFoundException, Param, ParseUUIDPipe, Req, Res} from '@nestjs/common';
+import {Controller, Get, NotFoundException, Req, Res} from '@nestjs/common';
 import { DevelopmentService } from './development.service';
 import { AuthService } from '../../auth/auth.service';
 import {UserService} from "../../database/user/user.service";
@@ -45,7 +45,7 @@ export class DevelopmentController {
   }
 
   @Get('dev/tags')
-  async getTags(@Req() req: Request) {
+  async getTags() {
     return await this.tagService._getAllTags()
   }
 
