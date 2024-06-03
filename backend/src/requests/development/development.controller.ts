5b1fbdb3525541412b66d3921cd015680f702c67
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-06-03 21:28:46 +0200

    BE: updated dev-API, Allows setting/unset isPro/isAdmin and get profile states Request to profile is now sending all information not shortened and with no preview!!!
    Signed-off-by: Integraluminium <95375836+Integraluminium@users.noreply.github.com>

--- backend/src/requests/development/development.controller.ts
+++ backend/src/requests/development/development.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {Controller, Get, NotFoundException, Param, ParseUUIDPipe, Req, Res} from '@nestjs/common';
 import { DevelopmentService } from './development.service';
 import { AuthService } from '../../auth/auth.service';
+import {UserService} from "../../database/user/user.service";
 
 @Controller()
 export class DevelopmentController {
@@ -8,13 +9,61 @@ export class DevelopmentController {
     //     private readonly services
     private readonly devService: DevelopmentService,
     private readonly authService: AuthService,
+    private readonly userService: UserService,
   ) {}
+
+  /**
+   * This request is only for development purposes
+   * Automatically upgrade user to proUser
+   * @param req
+   */
   @Get('upgrade')
-  upgradeThisUser() {
-    return 'not implemented';
+  async upgradeThisUser(@Req() req: any):Promise<boolean> {
+    return await this.userService.upgradeUser(req.userId);
   }
+
+  /**********/
+  @Get('dev/isPro')
+  async isProUser(@Req() req: any):Promise<boolean> {
+    return await this.userService.isProUser(req.userId);
+  }
+
+  @Get('dev/unsetProUser')
+  async unsetProUser(@Req() req: any):Promise<boolean> {
+    return await this.devService.unsetIsProUser(req.userId);
+  }
+  @Get('dev/setProUser')
+  async setProUser(@Req() req: any):Promise<boolean> {
+    return await this.userService.upgradeUser(req.userId);
+  }
+
+  @Get('dev/setAdmin')
+  async setAdmin(@Req() req: any):Promise<object> {
+    return await this.devService.setAdminAttribute(req.userId, true);
+  }
+
+  @Get('dev/unsetAdmin')
+  async unsetAdmin(@Req() req: any):Promise<object> {
+    return await this.devService.setAdminAttribute(req.userId, false);
+  }
+  @Get('dev/profile')
+  async profile(@Req() req: any):Promise<object> {
+    const profile = await this.userService.getUser(req.userId);
+    const cookie = req.headers.cookie;
+
+    if(profile == null) throw new NotFoundException('User not found');
+    return {
+      profile,
+      cookie
+    };
+  }
+
+  /**
+   * This request is only for development purposes
+   * Generic response for /
+   */
   @Get()
-  getBaseResponse(): string {
+  async getBaseResponse(): Promise<string> {
     return this.devService.getLoginResponse();
   }
 
