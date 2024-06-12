40fb7f8a230ad7938b754f2a6a9fc686d64bb6c0
Benjamin Loidl <benni@loidlnetz.de>
2024-06-12 15:58:16 +0200

    FE: Removing Mockup / Placeholder Elements as requested

--- frontend/src/pages/dashboard/profile/Profile.tsx
+++ frontend/src/pages/dashboard/profile/Profile.tsx
@@ -3,6 +3,7 @@ import "./Profile.scss";
 import completing from "../../../illustrations/completing.svg";
 import { useAlert } from "react-alert";
 import { axiosError } from "../../../def/axios-error";
+import Button from "../../../components/button/Button";
 
 /**
  * Renders the profile page, currently static
@@ -28,7 +29,7 @@ export default function Profile(props: {}) {
 					
 					<hr/>
 					<h3>Profile Picture</h3>
-					<div style={ { display: "flex", gap: "var(--spacing)" } }>
+					<div style={ { display: "flex", gap: "var(--spacing)", alignItems: "center" } }>
 						<img src={ "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" }
 							 alt={ "Avatar" }
 							 style={ {
@@ -37,10 +38,7 @@ export default function Profile(props: {}) {
 								 borderRadius: "50%",
 								 objectFit: "cover"
 							 } }/>
-						<button className={ "btn btn-glass" } style={ { alignSelf: "center" } }>
-							<i className={ "fi fi-rr-upload" }/>
-							<span>Upload</span>
-						</button>
+						<Button icon={ "fi fi-rr-upload" }>Upload</Button>
 					</div>
 				</div>
 				
@@ -54,31 +52,13 @@ export default function Profile(props: {}) {
 				<tbody>
 				<tr>
 					<td style={ { width: "50%", paddingRight: "calc(var(--spacing) / 2)" } }>
-						<label className={ "caption" } htmlFor={ "firstname" }>First Name</label>
-					</td>
-					<td style={ { width: "50%", paddingLeft: "calc(var(--spacing) / 2)" } }>
-						<label className={ "caption" } htmlFor={ "lastname" }>Last Name</label>
+						<label className={ "caption" } htmlFor={ "firstname" }>Username</label>
 					</td>
 				</tr>
 				<tr>
 					<td style={ { paddingRight: "calc(var(--spacing) / 2)" } }>
 						<input type={ "text" } className={ "form-input" } id={ "firstname" }
-							   placeholder={ "John" } defaultValue={ "Benni" } style={ { width: "100%" } }/>
-					</td>
-					<td style={ { paddingLeft: "calc(var(--spacing) / 2)" } }>
-						<input type={ "text" } className={ "form-input" } id={ "lastname" }
-							   placeholder={ "Doe" } defaultValue={ "Loidl" } style={ { width: "100%" } }/>
-					</td>
-				</tr>
-				<tr>
-					<td style={ { paddingRight: "calc(var(--spacing) / 2)" } }>
-						<label className={ "caption" } htmlFor={ "email" }>Email</label>
-					</td>
-				</tr>
-				<tr>
-					<td style={ { paddingRight: "calc(var(--spacing) / 2)" } }>
-						<input type={ "email" } className={ "form-input" } id={ "email" }
-							   placeholder={ "john.doe@gmail.com" } style={ { width: "100%" } }/>
+							   placeholder={ "John" } defaultValue={ "John Doe" } style={ { width: "100%" } }/>
 					</td>
 				</tr>
 				</tbody>
@@ -105,85 +85,6 @@ export default function Profile(props: {}) {
 					<p>Choose your expert topics to help others find you.</p>
 				</div>
 			</div>
-		</div>
-		
-		<div className={ "container profile-memberships" }>
-			<h2>
-				<i className={ "fi fi-rr-ranking-star" }/>
-				My Membership
-			</h2>
-			<p>Upgrade your membership to unlock more features.</p>
-			
-			<hr/>
-			
-			<table style={ { width: "100%", textAlign: "center" } }>
-				<tbody>
-				<tr>
-					<td style={ { width: "25%" } }></td>
-					<td style={ { width: "25%" } }></td>
-					<td style={ { width: "25%" } }></td>
-					<td className={ "caption" } style={ { width: "25%", } }>My Current Plan
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td className={ "plan" } style={ { background: "var(--background-color-secondary)" } }>Free</td>
-					<td className={ "plan" }
-						style={ { background: "var(--primary-color)", color: "var(--primary-color-contrast)" } }>
-						Premium
-					</td>
-					<td className={ "plan" }
-						style={ { background: "var(--primary-color)", color: "var(--primary-color-contrast)" } }>
-						Pro
-					</td>
-				</tr>
-				<tr>
-					<td style={ { textAlign: "left" } }>
-						<i className={ "fi fi-rr-question" }/>
-						Ask Questions
-					</td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-				</tr>
-				<tr>
-					<td style={ { textAlign: "left" } }>
-						<i className={ "fi fi-rr-comment-dots" }/>
-						Comment
-					</td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-					<td><i className={ "fi fi-rr-check" }/></td>
-				</tr>
-				<tr>
-					<td style={ { textAlign: "left" } }>
-						<i className={ "fi fi-rr-brain" }/>
-						AI Replies
-					</td>
-					<td>up to 3 / m</td>
-					<td><i className={ "fi fi-rr-infinity" }/></td>
-					<td><i className={ "fi fi-rr-infinity" }/></td>
-				</tr>
-				<tr>
-					<td style={ { textAlign: "left" } }>
-						<i className={ "fi fi-rr-wallet" }/>
-						Price
-					</td>
-					<td><p>
-						<span style={ { fontSize: "2em" } }>€0.00</span>
-						<span> / m</span>
-					</p></td>
-					<td><p>
-						<span style={ { fontSize: "2em" } }>€1.99</span>
-						<span> / m</span>
-					</p></td>
-					<td><p>
-						<span style={ { fontSize: "2em" } }>€4.99</span>
-						<span> / m</span>
-					</p></td>
-				</tr>
-				</tbody>
-			</table>
 		</div>
 	</>
 }
