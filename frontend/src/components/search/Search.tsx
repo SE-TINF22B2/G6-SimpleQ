47f7a00459b5931f6f84d62d3d961ca144452677
Benjamin Loidl <benni@loidlnetz.de>
2024-06-14 08:30:13 +0200

    FE: Implementing Search

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -2,9 +2,12 @@ import "./Search.scss";
 import axios, { CancelTokenSource } from "axios";
 import { useAlert } from "react-alert";
 import { useTranslation } from "react-i18next";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { axiosError } from "../../def/axios-error";
 import Modal from "react-responsive-modal";
+import { QuestionDef } from "../../def/QuestionDef";
+import Avatar from "../avatar/Avatar";
+import { formatDate } from "../../def/converter";
 
 let cancelToken: CancelTokenSource;
 
@@ -17,7 +20,7 @@ export default function Search(props: { isOpen: boolean, closeModal: () => void 
 	const alert = useAlert();
 	const { t } = useTranslation();
 	
-	const [suggestions, setSuggestions] = useState<any[]>([]);
+	const [suggestions, setSuggestions] = useState<QuestionDef[]>([]);
 	
 	const updateSuggestions = (query: string) => {
 		if (cancelToken) cancelToken.cancel("Operation canceled due to new request.");
@@ -30,8 +33,26 @@ export default function Search(props: { isOpen: boolean, closeModal: () => void 
 			cancelToken: cancelToken.token
 		})
 			  .then(res => {
-				  console.log(res, res.data);
-				  setSuggestions(res.data);
+				  let _suggestions: QuestionDef[] = [];
+				  res.data.forEach((_question: any) => _suggestions.push({
+					  answers: 0,
+					  author: {
+						  id: "def",
+						  name: "anonymous",
+						  type: "guest"
+					  },
+					  created: "0",
+					  dislikes: _question.dislikes ?? 0,
+					  id: "abc",
+					  isDiscussion: false,
+					  isFavorite: false,
+					  likes: _question.likes ?? 0,
+					  opinion: "none",
+					  tags: ["tag1", "tag2"],
+					  title: "title",
+					  updated: "0"
+				  }));
+				  setSuggestions(_suggestions);
 			  })
 			  .catch(err => {
 				  if (err.message.includes('new request')) return;
@@ -40,10 +61,14 @@ export default function Search(props: { isOpen: boolean, closeModal: () => void 
 			  });
 	}
 	
+	useEffect(() => {
+		setSuggestions([]);
+	}, [props.isOpen]);
+	
 	return <Modal open={ props.isOpen } onClose={ props.closeModal }
 				  classNames={ {
 					  overlay: "modal-overlay",
-					  modal: "modal-modal modal-no-padding",
+					  modal: "modal-modal modal-no-padding search",
 					  closeButton: "modal-close"
 				  } }
 				  styles={ { modal: { marginTop: "var(--spacing)" } } }>
@@ -71,7 +96,50 @@ export default function Search(props: { isOpen: boolean, closeModal: () => void 
 				<span className={ "caption" }>{ t('dashboard.search.info') }</span>
 			</p>
 			
-			<p>{ suggestions.length } suggestions.</p>
+			{ suggestions.map((suggestion, i) => <div key={ i } className={ "suggestion" } tabIndex={ 0 }>
+				<div className={ "suggestion-main" }>
+					<div className={ "suggestion-title" }>
+						{ suggestion.isDiscussion
+							? <span className={ "badge badge-outline" }>
+									<i className={ "fi fi-rr-comments-question" }
+									   style={ { marginRight: "calc(var(--spacing) / 2)" } }/>
+									Discussion
+							</span>
+							: <span className={ "badge badge-outline" }>
+									<i className={ "fi fi-rr-interrogation" }
+									   style={ { marginRight: "calc(var(--spacing) / 2)" } }/>
+									Question
+								</span>
+						}
+						<span className={ "suggestion-title-divider" }/>
+						<h2>{ suggestion.title }</h2>
+						<p className={ "badge" }>Tag2</p>
+					</div>
+					<p className={ "caption suggestion-caption" }>
+						<span>
+							<i className={ "fi fi-rr-clock" }/>
+							{ formatDate(suggestion.created) }
+						</span>
+						<span>·</span>
+						<span>
+							<i className={ "fi fi-rr-refresh" }/>
+							{ formatDate(suggestion.updated) }
+						</span>
+						<span>·</span>
+						<span>
+							<i className={ "fi fi-rr-social-network" }/>
+							{ suggestion.likes }
+						</span>
+						<span>·</span>
+						<span>
+							<i className={ "fi fi-rr-social-network flipY" }/>
+							{ suggestion.dislikes }
+						</span>
+					</p>
+				</div>
+				
+				<Avatar/>
+			</div>) }
 		</div>
 	</Modal>
 }
