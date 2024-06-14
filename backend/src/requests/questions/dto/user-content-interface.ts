0bd96705e052a5d56761b3edec434dd8b476f856
Integraluminium <95375836+Integraluminium@users.noreply.github.com>
2024-06-15 00:00:07 +0200

    BE: added Tag Date to Question
    * implemented getLastupdate database service
    * added Date to object with fallback
    * changed updated Type in IQuestion to Date
    
    Signed-off-by: Integraluminium <95375836+Integraluminium@users.noreply.github.com>
    (cherry picked from commit 65d2cc34eaf02db6ab02d06647b68e693fa65365)

--- backend/src/requests/questions/dto/user-content-interface.ts
+++ backend/src/requests/questions/dto/user-content-interface.ts
@@ -13,7 +13,7 @@ export interface IQuestion extends IUserContent {
   title: string;
   tags: string[];
   numberOfAnswers: number;
-  updated: string;
+  updated: Date;
   author: {
     id: string;
     name: string;
