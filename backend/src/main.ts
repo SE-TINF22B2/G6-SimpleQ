991989ec9419662e450a386cbce6a33735a5cc5d
benniloidl <benni@loidlnetz.de>
2024-06-14 18:09:04 +0200

    BE: Fixing Delete Favourites

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    methods: ['*'],
+    methods: ['*', 'DELETE'],
     credentials: true,
   });
   global.ory = new sdk.FrontendApi(
